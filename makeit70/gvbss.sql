4d43354034e48432ed1683dd927da11f8c809157
Ishan Mathur <49003172+i5m@users.noreply.github.com>
2019-07-29 12:13:12 +0530

    Add files via upload

--- makeit70/gvbss.sql
+++ makeit70/gvbss.sql
@@ -1,92 +1,91 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 27, 2019 at 12:38 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `gvbss`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `test1_2019_07_03`
---
-
-CREATE TABLE `test1_2019_07_03` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `ques` varchar(1300) DEFAULT NULL,
-  `opt1` varchar(210) DEFAULT NULL,
-  `opt2` varchar(210) DEFAULT NULL,
-  `opt3` varchar(210) DEFAULT NULL,
-  `opt4` varchar(210) DEFAULT NULL,
-  `correctans` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
+CREATE TABLE `users_third` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `isAdmin` int(2) DEFAULT '0',
-  `test1_2019_07_03` varchar(200) DEFAULT NULL
+  `fullname` varchar(60),
+  `ladd` varchar(100),
+  `padd` varchar(100),
+  `phone` varchar(15),
+  `pphone` varchar(15),
+  `emailid` varchar(50),
+  `tenth` varchar(10),
+  `twelveth` varchar(10),
+  `enrollno` varchar(20),
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `test1_2019_07_03`
---
-ALTER TABLE `test1_2019_07_03`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
+ALTER TABLE `users_third`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `test1_2019_07_03`
---
-ALTER TABLE `test1_2019_07_03`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
+ALTER TABLE `users_third`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE `users_fifth` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(60),
+  `ladd` varchar(100),
+  `padd` varchar(100),
+  `phone` varchar(15),
+  `pphone` varchar(15),
+  `emailid` varchar(50),
+  `tenth` varchar(10),
+  `twelveth` varchar(10),
+  `enrollno` varchar(20),
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `users_fifth`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+ALTER TABLE `users_fifth`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `users_seventh` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(60),
+  `ladd` varchar(100),
+  `padd` varchar(100),
+  `phone` varchar(15),
+  `pphone` varchar(15),
+  `emailid` varchar(50),
+  `tenth` varchar(10),
+  `twelveth` varchar(10),
+  `enrollno` varchar(20),
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `users_seventh`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+ALTER TABLE `users_seventh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+CREATE TABLE `users_admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `users_admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+ALTER TABLE `users_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
