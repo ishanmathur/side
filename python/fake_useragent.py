b4ffdefdca450302ea9c39deecd4e5a65674e6b5
Ishan Mathur <49003172+i5m@users.noreply.github.com>
2019-12-08 18:34:11 +0530

    Update fake_useragent.py

--- python/fake_useragent.py
+++ python/fake_useragent.py
@@ -6,18 +6,16 @@ class FakeUserAgentClass():
 
     def __init__(self):
         self.base_url = "https://fake-useragent.herokuapp.com/browsers/0.1.8"
-        
-        self.fake_useragent_file = open("fake_useragent_file.json", "r")
-        self.fake_useragent_list = []
         try:
-            self.data = json.load(self.fake_useragent_file)
-            for i in self.data["browsers"]:
-                for j in self.data["browsers"][i]:
-                    self.fake_useragent_list.append(j)
-            print(10*" >> ", "Got new Fake User-Agents !")
+            self.fake_useragent_file = open("fake_useragent_file.json", "r")
         except:
             self.get_list()
-            self.__init__()
+        self.fake_useragent_list = []
+        self.data = json.load(self.fake_useragent_file)
+        for i in self.data["browsers"]:
+            for j in self.data["browsers"][i]:
+                self.fake_useragent_list.append(j)
+        print(10*" >> ", "Got new Fake User-Agents !")
 
     def get_list(self):
         fake_useragent_file = open("fake_useragent_file.json", "w")
